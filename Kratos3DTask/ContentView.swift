7d7c1bafb62fc47410a591f8bfa911dff019d2bb
ahmetkasimnazli <ahmetnazl2@gmail.com>
2024-05-31 09:36:22 +0300

    Initial Commit

---
+++ Kratos3DTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kratos3DTask
+//
+//  Created by Ahmet Kasım Nazlı on 31.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
